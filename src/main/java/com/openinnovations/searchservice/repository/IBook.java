8569c8b648eeeef471feb13f785ceca041d7e77e
MartinSamanArata2018 <martin.saman@vallegrande.edu.pe>
2020-01-26 01:34:30 -0500

    :fire: Mejorando busquedas

--- src/main/java/com/openinnovations/searchservice/repository/IBook.java
+++ src/main/java/com/openinnovations/searchservice/repository/IBook.java
@@ -5,16 +5,21 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface IBook extends MongoRepository<Book, Long> {
 
-    List<Book> findAllByKeywordsContains(String category);
+    List<Book> findAllByKeywordsContains(List<String> keywords);
 
     List<Book> findAllByTitleContains(String title);
 
     List<Book> findAllByAuthorContains(String author);
 
     List<Book> findAllByCategoryContains(String category);
+
+    List<Book> findAllByKeywordsContainsOrTitleContainsOrAuthorContainsOrCategoryContains(List<String> keywords, String title, String author, String category);
+
+//    List<Book> findAllByTitleInOrAuthorInOrCategoryInOrKeywordsContains(List<String> title, List<String> author, List<String> category, List<String> keywords);
 }
--- src/main/java/com/openinnovations/searchservice/rest/BookRest.java
+++ src/main/java/com/openinnovations/searchservice/rest/BookRest.java
@@ -13,19 +13,8 @@ public class BookRest {
     @Autowired
     IBook repo;
 
-    @GetMapping
-    public List<Book> listarTodo() {
-        return repo.findAll();
-    }
-
-    //Solo para pruebas
-    @PostMapping
-    public Book registrar(@RequestBody Book book) {
-        return repo.save(book);
-    }
-
-    @GetMapping("/keywords/{keywords}")
-    public List<Book> buscarKeywords(@PathVariable("keywords") String keywords) {
+    @GetMapping("/keywords")
+    public List<Book> buscarKeywords(@RequestBody List<String> keywords) {
         return repo.findAllByKeywordsContains(keywords);
     }
 
@@ -42,5 +31,28 @@ public class BookRest {
     @GetMapping("/categoria/{categoria}")
     public List<Book> buscarCategoria(@PathVariable("categoria") String categoria) {
         return repo.findAllByCategoryContains(categoria);
+    }
+
+    //Solo para pruebas
+    @PostMapping
+    public List<Book> registrar(@RequestBody List<Book> books) {
+        for (Book book : books) {
+            repo.save(book);
+        }
+        return repo.findAll();
+    }
+
+    //Solo para pruebas
+    @DeleteMapping
+    public List<Book> borrarTodo() {
+        repo.deleteAll();
+        return repo.findAll();
+    }
+
+    //En Desarrollo
+    @GetMapping
+    public List<Book> listarTodo(@RequestBody List<String> palabras) {
+        System.out.println(palabras);
+        return repo.findAllByKeywordsContainsOrTitleContainsOrAuthorContainsOrCategoryContains(palabras, palabras.get(0), palabras.get(0), palabras.get(0));
     }
 }
